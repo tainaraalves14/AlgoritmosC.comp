721f65ad2ba4034e984d688af5cc6f13b527c3c5
Tainara Alves <tainara.nascimento16@etec.sp.gov.br>
2024-03-21 21:17:36 -0300

    create py one

---
+++ ex.one.py
@@ -0,0 +1,8 @@
+# Exercício 2
+# Escreva um programa para ler a base e a altura de um retângulo, calcular e escrever a sua área,
+# sabendo que área=base*altura
+# Lembre que o * é o operador de multiplicação 
+
+# Exercício 3
+# Escreva um programa para ler uma temperatura em graus Celsius, calcular e escrever o valor correspondente em graus Fahrenheit. 
+# Para converter graus Celsius em graus Fahrenheit, multiplique por 1.8 e adicione 32, ou seja: °F = °C × 1.8 + 32
