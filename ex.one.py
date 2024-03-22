60aea5330f1420dab38336987ca9e3a3e47c4ac8
Tainara Alves <tainara.nascimento16@etec.sp.gov.br>
2024-03-21 21:26:00 -0300

    lista teste one

--- ex.one.py
+++ ex.one.py
@@ -6,3 +6,5 @@
 # Exercício 3
 # Escreva um programa para ler uma temperatura em graus Celsius, calcular e escrever o valor correspondente em graus Fahrenheit. 
 # Para converter graus Celsius em graus Fahrenheit, multiplique por 1.8 e adicione 32, ou seja: °F = °C × 1.8 + 32
+
+
